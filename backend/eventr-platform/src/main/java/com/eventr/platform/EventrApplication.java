db783da7e681eab98be2d3ab89ddc2141959eb46
Abhav Goel <goelabhav2002@gmail.com>
2026-01-20 22:35:52 +0530

    project setup

---
+++ backend/eventr-platform/src/main/java/com/eventr/platform/EventrApplication.java
@@ -0,0 +1,13 @@
+package com.eventr.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventrPlatformApplication.class, args);
+	}
+
+}
---
+++ backend/eventr-platform/src/test/java/com/eventr/platform/EventrPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventr.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventrPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
