147bf6cd9d30e2989ac13d98e545728cf24f820b
Abhav Goel <goelabhav2002@gmail.com>
2026-01-20 23:18:20 +0530

    corrected class name

--- backend/eventr-platform/src/main/java/com/eventr/platform/EventrApplication.java
+++ backend/eventr-platform/src/main/java/com/eventr/platform/EventrApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EventrApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EventrPlatformApplication.class, args);
+		SpringApplication.run(EventrApplication.class, args);
 	}
 
 }
